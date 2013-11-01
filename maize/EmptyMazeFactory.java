16e0a0574d7eaf99f24aa29a2fd8f9143c1fb4e0
Steve Wattam <stephenwattam@gmail.com>
2013-11-01 15:56:24 +0000

    Fixed longstanding bug with the empty maze generator whereby the end would overlap the start

--- maize/EmptyMazeFactory.java
+++ maize/EmptyMazeFactory.java
@@ -1,5 +1,7 @@
 package maize;
 
+import java.awt.Point;
+
 /** Creates an empty maze for the purposes of testing */
 public class EmptyMazeFactory implements MazeFactory {
 
@@ -26,8 +28,14 @@ public class EmptyMazeFactory implements MazeFactory {
             for(int j = 0; j < height; j++)
                 data[i][j] = (i == 0 || j == 0 || i == width-1 || j == height-1)? true : false;
   */          
+
+        Point end = new Point(width - 2, height - 2);
+        Point start = null;
+        while(start == null || start.equals(end)){
+            start = new Point(1 + (int)(Math.random() * width-3), 1 + (int)(Math.random() * height-3));
+        }
        
         // Create maze
-        return new Maze(data, 1 + (int)(Math.random() * width-3), 1 + (int)(Math.random() * height-3), width-2, height-2);
+        return new Maze(data, start.x, start.y, end.x, end.y);
     } 
 }
