f268e354d399ebd4cf324c4e9d82b2ce9c294d7a
John Vidler <j.vidler@lancaster.ac.uk>
2015-12-31 17:00:40 +0000

    A vaguely working JFlap graph reading bot!

--- bots/JFlapBot.java
+++ bots/JFlapBot.java
@@ -4,6 +4,7 @@ import maize.*;
 import org.w3c.dom.*;
 import org.xml.sax.*;
 import java.util.*;
+import java.util.concurrent.*;
 import java.io.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -26,23 +27,13 @@ public class JFlapBot extends JFrame implements Bot {
 		State mTo;
 		ArrayList<Character> mPush = new ArrayList<>();
 		ArrayList<Character> mPop = new ArrayList<>();
-
-		public Transition() {
-
-		}
 	}
 
 	public class State {
 		ArrayList<Transition> mArcs = new ArrayList<>();
 		int mID = -1;
 		String mName = null;
-
-		public State() {
-		}
 	}
-
-	private TreeMap<Integer,State>        mKnownStates      = new TreeMap<>();
-	
 
 	private String getNodeValue_r( Node root, ArrayList<String> nodespec, String _default ) {
 		if( nodespec.size() > 0 ) {
@@ -64,7 +55,11 @@ public class JFlapBot extends JFrame implements Bot {
 	}
 
 	private String getNodeValue( Node root, String nodespec, String _default ) {
-		return getNodeValue_r( root, new ArrayList<String>(Arrays.asList(nodespec.split( "\\." )) ), _default );
+		String value = getNodeValue_r( root, new ArrayList<String>(Arrays.asList(nodespec.split( "\\." )) ), _default );
+
+		System.out.println( "{" +nodespec+ " = " +(value==null?"null":value)+ "}" );
+
+		return value;
 	}
 
 	private Graph loadJFlapFile( File file ) {
@@ -92,18 +87,27 @@ public class JFlapBot extends JFrame implements Bot {
 					newState.mName = states.item(i).getAttributes().getNamedItem( "name" ).getTextContent();
 
 					graph.mStates.put( newState.mID, newState );
+					System.out.println( "State(" +newState.mID+ ")" );
 
 					if( getNodeValue(states.item(i), "initial", null ) != null )
 						graph.mStart = newState;
 
-					if( getNodeValue(states.item(i), "initial", null ) != null )
+					if( getNodeValue(states.item(i), "final", null ) != null )
 						graph.mFinish.add( newState );
 				}
    
 				for( int i=0; i<arcs.getLength(); i++ ) {
 					Transition transition = new Transition();
-					transition.mTo   = mKnownStates.get( Integer.parseInt( getNodeValue( arcs.item(i), "to",   "-1" ) ) );
-					transition.mFrom = mKnownStates.get( Integer.parseInt( getNodeValue( arcs.item(i), "from", "-1" ) ) );
+
+					transition.mTo   = graph.mStates.get( Integer.parseInt( getNodeValue( arcs.item(i), "to",   "-1" ) ) );
+
+					transition.mFrom = graph.mStates.get( Integer.parseInt( getNodeValue( arcs.item(i), "from", "-1" ) ) );
+
+					System.out.println( transition );
+					System.out.println( transition.mFrom );
+					System.out.println( transition.mFrom.mArcs );
+
+					transition.mFrom.mArcs.add( transition );
 
 					transition.mPush = new ArrayList<Character>();
 					for( char c : getNodeValue( arcs.item(i), "push", null ).toCharArray() )
@@ -144,7 +148,7 @@ public class JFlapBot extends JFrame implements Bot {
 		            File file = fc.getSelectedFile();
 		            
 		            System.out.println( "Opening: " + file.getName() + ".\n" );
-		            Graph graph = loadJFlapFile( file );
+		            mCurrentGraph = loadJFlapFile( file );
 		        }
     		}
     	} );
@@ -154,6 +158,14 @@ public class JFlapBot extends JFrame implements Bot {
 		setVisible( true );
 	}
 
+
+	// ///////////////////////// //
+	// ACTUAL BOT IMPLEMENTATION //
+	// ///////////////////////// //
+	ConcurrentLinkedQueue<Integer> mActionList = new ConcurrentLinkedQueue<Integer>();
+	Graph mCurrentGraph = null;
+	State mCurrentState = null;
+	Stack<Character> mStack = null;
 
 
 	/** Implementation of the Bot interface.
@@ -172,6 +184,82 @@ public class JFlapBot extends JFrame implements Bot {
      */
     @Override
     public int nextMove(boolean[][] view, int x, int y, int o, int fx, int fy) {
+    	if( mActionList.size() > 0 )
+    		return mActionList.remove();
+
+    	if( mCurrentGraph != null )
+    	{
+    		System.out.println( "[START] -> " );
+
+    		do {
+    			System.out.print( mCurrentState.mName + " -> " );
+
+	    		// Load sensor data //
+	    		ArrayList<Character> mInputTape = new ArrayList<>();
+
+
+
+	    		// Reset stack //
+	    		mStack = new Stack<>();
+
+
+	    		ArrayList<Transition> matches = new ArrayList<>();
+	    		for( Transition t : mCurrentState.mArcs ) {
+	    			if( t.mPop.size() == 0 )
+	    				matches.add( t );
+	    		}
+
+	    		int index = (int)(Math.random() * (matches.size()-1));
+	    		Transition action = matches.get( index );
+
+	    		for( Character c : action.mPop ) {
+	    			if( !(""+c).equalsIgnoreCase( ""+mStack.peek()) )
+	    				System.out.println( "SANITY CHECK FAIL: " +c+ " IS NOT " + mStack.peek() );
+
+	    			mStack.pop();
+	    		}
+
+	    		for( Character c : action.mPush ) {
+	    			mStack.push( c );
+	    		}
+
+	    		while( mStack.size() > 0 )
+	    		{
+	    			char c = Character.toLowerCase( mStack.pop() );
+
+	    			switch( c )
+	    			{
+	    				case 'f':
+	    					mActionList.add( Direction.FORWARD );
+	    					System.out.print( " {FWD} " );
+	    					break;
+
+	    				case 'b':
+	    					mActionList.add( Direction.BACK );
+	    					System.out.print( " {BACK} " );
+	    					break;
+
+	    				case 'l':
+	    					mActionList.add( Direction.LEFT );
+	    					System.out.print( " {LEFT} " );
+	    					break;
+
+	    				case 'r':
+	    					mActionList.add( Direction.RIGHT );
+	    					System.out.print( " {RIGHT} " );
+	    					break;
+
+	    				default:
+	    					System.out.println( "Invalid action: '" +c+ "'" );
+	    			}
+	    		}
+
+	    		mCurrentState = action.mTo;
+	    	} while( !mCurrentGraph.mFinish.contains( mCurrentState ) );
+	    	mCurrentState = mCurrentGraph.mStart;
+
+	    	System.out.println( "[FINISH]" );
+    	}
     	return Direction.FORWARD;
     }
 
@@ -196,7 +284,9 @@ public class JFlapBot extends JFrame implements Bot {
 
     @Override
     public void start(){
-    	
+    	if( mCurrentGraph != null ) {
+    		mCurrentState = mCurrentGraph.mStart;
+    	}
     }
 
     @Override
