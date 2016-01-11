6f38a04bf66e39b85d896a373eb217f048d42332
John Vidler <j.vidler@lancaster.ac.uk>
2016-01-11 00:54:30 +0000

    Edited example sources

--- bots/FSMBot/Graph.java
+++ bots/FSMBot/Graph.java
@@ -1,9 +1,13 @@
-import maize;
+import bots.FSMBot.State;
+import bots.FSMBot.Transition;
+
+import java.util.ArrayList;
+import java.util.TreeMap;
 
 public class Graph
 {
     public State mStart;
-    public ArrayList<State>        mFinish      = new ArrayList<>();
-    public TreeMap<Integer,State>  mStates      = new TreeMap<>();
+    public ArrayList<State> mFinish      = new ArrayList<>();
+    public TreeMap<Integer,State> mStates      = new TreeMap<>();
     public ArrayList<Transition>   mTransitions = new ArrayList<>();
 }
